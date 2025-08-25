6f9d63160733ae877570d1441cbd68c38eb3c1dc
Kimleng <kimleng-lim@outlook.com>
2025-08-25 16:14:12 -0400

    add signalr and client test page

---
+++ YoutubeDownloader.WebApi/Program.cs
@@ -0,0 +1,36 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy( policy => 
+        policy.WithOrigins("http://127.0.0.1:5500")
+           .AllowAnyMethod()
+           .AllowAnyHeader()
+           .AllowCredentials());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors();
+app.MapGet("/", () => Results.Ok("Welcome to the Youtube Downloader Web API!"))
+.WithName("Quick health check")
+.WithOpenApi();
+
+app.MapHub<TestHub>("/hubs/test");
+
+
+app.Run();
