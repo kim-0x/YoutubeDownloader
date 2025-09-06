c3c48bbe73cc0a17ee52587fa3e210010aff1c68
Kimleng <kimleng-lim@outlook.com>
2025-09-06 14:02:25 -0400

    fetch video title and update cache when downloading

--- YoutubeDownloader.WebApi/Controller/DownloadController.cs
+++ YoutubeDownloader.WebApi/Controller/DownloadController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 [ApiController]
 [Route("api/[controller]")]
@@ -6,12 +7,15 @@ public class DownloadController : ControllerBase
 {
     private readonly AudioDownloadService _audioDownloadService;
     private readonly IOutputStorage _outputStorage;
-
-    public DownloadController(AudioDownloadService audioDownloadService,
-       IOutputStorage outputStorage)
+    private readonly IMemoryCache _memoryCache;
+    public DownloadController(
+        AudioDownloadService audioDownloadService,
+        IOutputStorage outputStorage,
+        IMemoryCache memoryCache)
     {
         _audioDownloadService = audioDownloadService;
         _outputStorage = outputStorage;
+        _memoryCache = memoryCache;
     }
 
     [HttpPost]
@@ -30,6 +34,20 @@ public class DownloadController : ControllerBase
                 StartAt: model.StartAt,
                 EndAt: model.EndAt
             );
+
+            if (model.Title is not null)
+            {
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(5));
+                if (_memoryCache.TryGetValue<VideoModel>(model.VideoUrl, out var videoInfo))
+                {
+                    if (videoInfo is not null)
+                    {
+                        videoInfo.Title = model.Title;
+                        _memoryCache.Set(model.VideoUrl, videoInfo, cacheEntryOptions);
+                    }
+                }
+            }
 
             var _ = Task.Run(async () => await _audioDownloadService.ExecuteAsync(request));
             return Ok("Request accepted. Processing download.");
