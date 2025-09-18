28bcc41bf316f4e2788ea4474e8c0996e4ff88a8
Kimleng <kimleng-lim@outlook.com>
2025-09-18 09:54:01 -0400

    Add song list component

--- youtube-downloader-webui/src/app/app.component.ts
+++ youtube-downloader-webui/src/app/app.component.ts
@@ -1,21 +1,11 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-  OnDestroy,
-  OnInit,
-} from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { VideoFormComponent } from './video/form/video-form.component';
 import { AudioPlayerComponent } from './audio/player/audio-player.component';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { ScrollingModule } from '@angular/cdk/scrolling';
-import { AsyncPipe, NgIf } from '@angular/common';
-import { SongService } from './service/song.service';
-import { DownloadEventsService } from './service/download-events.service';
-import { map, Subscription, withLatestFrom } from 'rxjs';
-import { ReportService } from './service/report.service';
+import { SongListComponent } from './song/list/song-list.component';
 
 @Component({
   selector: 'app-root',
@@ -26,43 +16,9 @@ import { ReportService } from './service/report.service';
     ScrollingModule,
     VideoFormComponent,
     AudioPlayerComponent,
-    NgIf,
-    AsyncPipe,
+    SongListComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnInit, OnDestroy {
-  private readonly _subscription = new Subscription();
-
-  private readonly _songService = inject(SongService);
-  private readonly _downloadEventsService = inject(DownloadEventsService);
-  private readonly _reportService = inject(ReportService);
-  private readonly _newSong$ = this._downloadEventsService.completed$.pipe(
-    withLatestFrom(this._reportService.currentVideo$),
-    map(([url, currentVideo]) => ({
-      audioUrl: url,
-      title: currentVideo.title,
-    }))
-  );
-
-  readonly songs$ = this._songService.songs$;
-
-  ngOnInit(): void {
-    this._subscription.add(
-      this._newSong$.subscribe(async (newSong) => {
-        await this._songService.dispatchSongUpdate(newSong);
-        this._songService.updateTitleSelection(newSong.title);
-      })
-    );
-  }
-
-  onClick(title: string | undefined) {
-    this._songService.updateTitleSelection(title);
-  }
-
-  ngOnDestroy(): void {
-    this._subscription.unsubscribe();
-  }
-}
+export class AppComponent {}
---
+++ youtube-downloader-webui/src/app/song/list/song-list.component.ts
@@ -0,0 +1,50 @@
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatDivider } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { map, Subscription, withLatestFrom } from 'rxjs';
+import { DownloadEventsService } from '../../service/download-events.service';
+import { SongService } from '../../service/song.service';
+import { ReportService } from '../../service/report.service';
+
+@Component({
+  selector: 'app-song-list',
+  templateUrl: './song-list.component.html',
+  styleUrl: './song-list.component.scss',
+  imports: [NgIf, AsyncPipe, ScrollingModule, MatIconModule, MatDivider],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SongListComponent {
+  private readonly _subscription = new Subscription();
+
+  private readonly _songService = inject(SongService);
+  private readonly _downloadEventsService = inject(DownloadEventsService);
+  private readonly _reportService = inject(ReportService);
+  private readonly _newSong$ = this._downloadEventsService.completed$.pipe(
+    withLatestFrom(this._reportService.currentVideo$),
+    map(([url, currentVideo]) => ({
+      audioUrl: url,
+      title: currentVideo.title,
+    }))
+  );
+
+  readonly songs$ = this._songService.songs$;
+
+  ngOnInit(): void {
+    this._subscription.add(
+      this._newSong$.subscribe(async (newSong) => {
+        await this._songService.dispatchSongUpdate(newSong);
+        this._songService.updateTitleSelection(newSong.title);
+      })
+    );
+  }
+
+  onClick(title: string | undefined) {
+    this._songService.updateTitleSelection(title);
+  }
+
+  ngOnDestroy(): void {
+    this._subscription.unsubscribe();
+  }
+}
