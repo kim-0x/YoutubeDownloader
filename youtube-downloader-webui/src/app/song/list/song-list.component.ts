6c85d727c0aee8f9525510c62ce7de3ec728d2d7
Kimleng <kimleng-lim@outlook.com>
2025-09-24 16:48:09 -0400

    select first or default song in the list

--- youtube-downloader-webui/src/app/song/list/song-list.component.ts
+++ youtube-downloader-webui/src/app/song/list/song-list.component.ts
@@ -1,9 +1,11 @@
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
   inject,
+  OnDestroy,
   OnInit,
 } from '@angular/core';
 import { MatDivider } from '@angular/material/divider';
@@ -12,10 +14,12 @@ import { MatRippleModule } from '@angular/material/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../store/state';
 import {
+  firstOrDefaultSongSelector,
   selectedSongTitleSelector,
   songItemsSelector,
 } from '../../store/song/song.selector';
 import { SongActionTypes } from '../../store/song/song.actions';
+import { filter, Subscription, take } from 'rxjs';
 
 @Component({
   selector: 'app-song-list',
@@ -32,9 +36,12 @@ import { SongActionTypes } from '../../store/song/song.actions';
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SongListComponent implements OnInit {
+export class SongListComponent implements OnInit, AfterViewInit, OnDestroy {
   private readonly _store = inject(Store<AppState>);
   readonly songs$ = this._store.select(songItemsSelector);
+  readonly firstOrDefaultSong$ = this._store.select(firstOrDefaultSongSelector);
+
+  private readonly _subscription = new Subscription();
 
   readonly selectedSongTitle$ = this._store.select(selectedSongTitleSelector);
 
@@ -42,7 +49,24 @@ export class SongListComponent implements OnInit {
     this._store.dispatch({ type: SongActionTypes.LoadSongs });
   }
 
+  ngAfterViewInit(): void {
+    this._subscription.add(
+      this.firstOrDefaultSong$
+        .pipe(
+          filter((x) => x.title !== ''),
+          take(1)
+        )
+        .subscribe(({ title }) => {
+          this._store.dispatch({ type: SongActionTypes.SelectSong, title });
+        })
+    );
+  }
+
   onClick(title: string) {
     this._store.dispatch({ type: SongActionTypes.SelectSong, title });
+  }
+
+  ngOnDestroy(): void {
+    this._subscription.unsubscribe();
   }
 }
--- youtube-downloader-webui/src/app/store/song/song.selector.ts
+++ youtube-downloader-webui/src/app/store/song/song.selector.ts
@@ -1,6 +1,7 @@
 import { createSelector } from '@ngrx/store';
 import { AppState } from '../state';
 import { SongState } from '../state/song.state';
+import { SongDetail, SongItem } from '../state/song.model';
 
 export const songSelector = (appState: AppState) => appState.song;
 
@@ -12,6 +13,20 @@ export const songItemsSelector = createSelector(
 export const selectedSongSelector = createSelector(
   songSelector,
   (song: SongState) => song.selected
+);
+
+export const firstOrDefaultSongSelector = createSelector(
+  songItemsSelector,
+  (items: SongItem[]) => {
+    let detail: SongDetail = { title: '', audioUrl: '' };
+
+    if (items.length > 0 && items[0].songDetails.length > 0) {
+      detail.title = items[0].songDetails[0].title;
+      detail.audioUrl = items[0].songDetails[0].audioUrl;
+    }
+
+    return detail;
+  }
 );
 
 export const selectedSongTitleSelector = createSelector(
