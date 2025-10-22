5c28f56a6a5b5466b45150d746b0818d2917a779
Kimleng <kimleng-lim@outlook.com>
2025-10-22 10:47:12 -0400

    fix date label this week

--- YoutubeDownloader.WebApi/Utils/DateHelper.cs
+++ YoutubeDownloader.WebApi/Utils/DateHelper.cs
@@ -5,7 +5,7 @@ public static class DateHelper
     public static string ToDateLabel(this string inputDate)
     {
         var today = DateTime.Today;
-        
+
         if (!DateTimeOffset.TryParse(inputDate, CultureInfo.InvariantCulture, DateTimeStyles.AssumeLocal, out var dto)
          && !DateTimeOffset.TryParse(inputDate, CultureInfo.CurrentCulture, DateTimeStyles.AssumeLocal, out dto))
         {
@@ -13,20 +13,38 @@ public static class DateHelper
         }
 
         var date = dto.ToLocalTime().Date;
-        if (date == today)
-        {
-            return "Today";
-        }
-        else if (date == today.AddDays(-1))
-        {
-            return "Yesterday";
-        }
-        else
-        {
-            var isThisWeek = Enumerable.Range(2, 5)
-                .Any(day => date == today.AddDays(Math.Sign(-1) * day));
+        if (date == today) return "Today";
 
-            return isThisWeek ? "This Week" : "Old";
+        if (date == today.AddDays(-1)) return "Yesterday";
+
+        var weekOfYear = ISOWeek.GetWeekOfYear(today);
+        var year = date.Year;
+        var startAndEndDateOfWeek = GetStartAndEndDateOfWeek(year, weekOfYear);        
+        if (date >= startAndEndDateOfWeek.Start && date <= startAndEndDateOfWeek.End)
+        {
+            return "This Week";
         }
+
+        if (date > startAndEndDateOfWeek.End)
+        {
+            return "Future";
+        }
+
+        return "Old";
+    }
+    
+    public static (DateTime Start, DateTime End) GetStartAndEndDateOfWeek(int year, int weekOfYear, DayOfWeek firstDayOfWeek = DayOfWeek.Monday)
+    {
+        var firstDayOfYear = new DateTime(year, 1, 1);
+        var dayOffset = (int)firstDayOfWeek - (int)firstDayOfYear.DayOfWeek;
+        if (dayOffset > 0)
+        {
+            dayOffset -= 7;
+        }
+
+        var firstDayOfFirstWeek = firstDayOfYear.AddDays(dayOffset);
+        var startDayOfTheWeek = firstDayOfFirstWeek.AddDays((weekOfYear - 1) * 7);
+        var endDayOfTheWeek = startDayOfTheWeek.AddDays(6);
+        return (startDayOfTheWeek, endDayOfTheWeek);
     }
 }
